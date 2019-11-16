633f06faef2387ff91175081e8fe63921fa27706
Brandon El Haj <brandon.elhaj@gmail.com>
2019-11-15 21:48:15 -0800

    New Services Page

--- services.php
+++ services.php
@@ -12,8 +12,23 @@ Programmer: Brandon El Haj
 <main>
 	<!-- Services Section -->
 	<section id="services">
-		<!-- Services infographic. SVG file with dimensions 518px by 853px. -->
-		<img src="/assets/services.svg" alt="Rates and Appointments infographic" class="services">
+		<div class="service_header">
+			<h1 class="upper_service_title">Your</h1>
+			<h1 class="lower_service_title">Appointment</h1>
+		</div>
+		<section class="appointment_wrapper">
+			<div class="item1"><img src="/assets/icon_90min.svg" class="intake_icon"></div>
+			<div class="item2">
+				<h2>INTAKE</h2>
+				<p>Prior to the first appointment you'll be asked to complete an in-depth questionnaire and blood work to assist Tracy in identifying nutritional, lifestyle, gastrointestinal and hormonal factors that may be contributing to your symptoms. Then entire assessment takes about five hours, of which about 2 hours are face to face time with Tracy.
+				</p>
+			</div>
+			<div class="item3"><img src="/assets/icon_50min.svg" class="follow_up_icon"></div>
+			<div class="item4">
+				<h2>FOLLOW-UP</h2>
+				<p>Tracy will work with you to change the lifestyle and dietary factors that are contributing to your symptoms</p>
+			</div>
+		</section>	
 		<!-- Schedule Button. SVG file with dimensions 171px by 34px. -->
 		<a href="https://www.pipsychiatry.com/contact.php" id="book_appt_button"></a>	
 	</section>
