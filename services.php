778d3c9df038f8e5738161289cc5100c363fd8c7
Brandon El Haj <brandon.elhaj@gmail.com>
2019-11-16 20:54:23 -0800

    Updated Services Page

--- services.php
+++ services.php
@@ -17,18 +17,51 @@ Programmer: Brandon El Haj
 			<h1 class="lower_service_title">Appointment</h1>
 		</div>
 		<section class="appointment_wrapper">
-			<div class="item1"><img src="/assets/icon_90min.svg" class="intake_icon"></div>
-			<div class="item2">
-				<h2>INTAKE</h2>
-				<p>Prior to the first appointment you'll be asked to complete an in-depth questionnaire and blood work to assist Tracy in identifying nutritional, lifestyle, gastrointestinal and hormonal factors that may be contributing to your symptoms. Then entire assessment takes about five hours, of which about 2 hours are face to face time with Tracy.
-				</p>
+			<div class="row">
+				<div class="column"><img src="/assets/icon_90min.svg" class="intake_icon" alt="90 to 120 mins appointment icon"></div>
+				<div class="column_left">
+					<h2 class="appoint_title">INTAKE</h2>
+					<p class="appoint_paragraph">Prior to the first appointment you'll be asked to complete an in-depth questionnaire and blood work to assist Tracy in identifying nutritional, lifestyle, gastrointestinal and hormonal factors that may be contributing to your symptoms. Then entire assessment takes about five hours, of which about 2 hours are face to face time with Tracy.
+					</p>
+				</div>
+				<div class="column"><img src="/assets/icon_50min.svg" class="follow_up_icon" alt="50 mins appointment icon"></div>
+				<div class="column_left">
+					<h2 class="appoint_title appoint_tab">FOLLOW-UP</h2>
+					<p class="appoint_paragraph">Tracy will work with you to change the lifestyle and dietary factors that are contributing to your symptoms</p>
+				</div>
 			</div>
-			<div class="item3"><img src="/assets/icon_50min.svg" class="follow_up_icon"></div>
-			<div class="item4">
-				<h2>FOLLOW-UP</h2>
-				<p>Tracy will work with you to change the lifestyle and dietary factors that are contributing to your symptoms</p>
-			</div>
-		</section>	
+		</section>
+		<section class="appointment_details_wrapper">
+			<div class="row pad_top">
+				<div class="column column_tab">
+					<img src="/assets/icon_meds.svg" class="meds_icon" alt="medicine bottles icon">
+					<h2 class="appoint_detail_title">SUPPLEMENTS,</h2>
+					<h2 class="appoint_detail_title">BOTANICAL, &</h2>
+					<h2 class="appoint_detail_title">PRESCRIBED MEDICATION</h2>
+					<p class="appoint_detail_paragraph">Tracy utilizes supplements, botanicals, and prescribed medications when necessary.</p>
+				</div>
+				<div class="column column_tab">
+					<img src="/assets/icon_network.svg" class="network_icon" alt="health insurance card network icon">
+					<h2 class="appoint_detail_title">NETWORK</h2>
+					<p class="appoint_detail_paragraph">Tracy is in network with BCBS/Regence, MODA, Pacificsource and Lifewise.</p>
+				</div>		
+			</div>	
+			<div class="row pad_bottom">
+				<div class="column column_tab">
+					<img src="/assets/icon_calendar.svg" class="calendar_icon" alt="calendar for monthly subscription icon">
+					<h2 class="appoint_detail_title">MONTHLY SUBSCRIPTION</h2>
+					<p class="appoint_detail_paragraph">Includes access to online health data storage and communication platform which enables you to interact with Tracy about your health goals and receive education and support</p>
+				</div>
+				<div class="column column_tab">
+					<img src="/assets/icon_wallet.svg" class="wallet_icon" alt="wallet icon">
+					<h2 class="appoint_detail_title">PRICING</h2>
+					<p class="appoint_detail_paragraph">Portions of your care may be billable to insurance. The consultation and subscription fees are designed to be HSA/FSA/HRA eligible, which means this could be fully funded by your healthcare plan.</p>
+				</div>		
+			</div>	
+		</section>
+		<section class="billing_info">
+			<p>To find out more about pricing and insurance<br/>reimbursements, please contact:<br/><span>Mind Ease Billing Company</span><br/><a href="tel:+19715339911">971.533.9911</a><br/><a href="mailto:benefits@mindeasebilling.com">benefits@mindeasebilling.com</a></p>
+		</section>			
 		<!-- Schedule Button. SVG file with dimensions 171px by 34px. -->
 		<a href="https://www.pipsychiatry.com/contact.php" id="book_appt_button"></a>	
 	</section>
